d3a794dc21532f80024501ac78c1bb6de16f5285
lathieuhiep <khacdiepkma90@gmail.com>
2024-06-09 10:34:24 +0700

    update code

--- src/com/neos/main/Main.java
+++ src/com/neos/main/Main.java
@@ -5,5 +5,15 @@ import com.neos.qldb.QuanLy;
 public class Main {
     public static void main(String[] args) {
         QuanLy quanLy = new QuanLy();
+
+        quanLy.themDB(0, "Thang", "123");
+        quanLy.themDB(1, "TranThanh", "43534");
+//        quanLy.themDB(2, "Hoai", "0153");
+        quanLy.themDB(3, "ToHoang", "345345");
+        quanLy.themDB(4, "ToThang", "43534");
+        quanLy.themDB(4, "ToTien", "34534");
+
+        quanLy.inDB();
+        quanLy.suaDB("43534", "11111");
     }
 }
--- src/com/neos/qldb/QuanLy.java
+++ src/com/neos/qldb/QuanLy.java
@@ -7,20 +7,20 @@ public class QuanLy {
         arrDB = new DanhBa[5];
     }
 
-    public boolean KiemTraTonTai(String sdt) {
+    public int kiemTraTonTai(String sdt) {
         for (int i = 0; i < arrDB.length; i++) {
             DanhBa db = arrDB[i];
 
             if ( db != null && db.getSdt().equals(sdt) ) {
-                return true;
+                return i;
             }
         }
 
-        return false;
+        return -1;
     }
 
-    public void ThemDanhBa(int index, String ten, String sdt) {
-        if (KiemTraTonTai(sdt)) {
+    public void themDB(int index, String ten, String sdt) {
+        if (kiemTraTonTai(sdt) >= 0) {
             System.out.println("Đã tồn tại SDT này: " + sdt);
             return;
         }
@@ -30,14 +30,37 @@ public class QuanLy {
     }
 
     public void inDB() {
+        System.out.println("Danh bạ của tôi!");
+
         for (int i = 0; i < arrDB.length; i++) {
             DanhBa db = arrDB[i];
 
             if ( db == null  ) {
                 System.out.println("DB " + (i+1) + " Trống!");
             } else {
-                System.out.println("DB " + (i + 1) + db);
+                System.out.println("DB " + (i + 1) + " " + db);
             }
         }
+    }
+
+    public void suaDB(String sdtCu, String sdtMoi) {
+        int viTri = kiemTraTonTai(sdtCu);
+
+        if (viTri == -1) {
+            System.out.println("Không tồn tại danh bạ: " + sdtCu);
+
+            return;
+        }
+
+        int viTriSDTMoi = kiemTraTonTai(sdtMoi);
+
+        if (viTriSDTMoi >=0 ) {
+            System.out.println("Đã tồn tại danh bạ: " + sdtMoi + " => không được phép sửa");
+            return;
+        }
+
+        arrDB[viTri].setSdt(sdtMoi);
+        System.out.println("\nSửa thành công");
+        inDB();
     }
 }
